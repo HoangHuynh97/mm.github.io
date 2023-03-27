cd39dc350c0125163d0cc3821575af632238f292
HoangHuynh97 <hoangdra97@gmail.com>
2023-03-27 15:54:02 +0700

    a

---
+++ app/Views/home/index.php
@@ -0,0 +1,15 @@
+<?php echo loadHeader(); ?>
+<body>
+	<div class="landing-container">
+		<div class="note">
+			Xảy ra lỗi từ phía back-end, vui lòng xem video demo. Xin lỗi vì sự bất tiện này!
+		</div>
+		<video controls class="video-inpage">
+		  	<source src="public/assets/images/1.mp4" type="video/mp4">
+		</video>
+	</div>
+</body>
+<script type="text/javascript">
+	
+</script>
+<?php echo loadFooter(); ?>

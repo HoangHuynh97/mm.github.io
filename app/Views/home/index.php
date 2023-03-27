b3404f120ee2a688db3072de144237dadb418184
HoangHuynh97 <hoangdra97@gmail.com>
2023-03-27 16:00:59 +0700

    update

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -4,9 +4,7 @@
 		<div class="note">
 			Xảy ra lỗi từ phía back-end, vui lòng xem video demo. Xin lỗi vì sự bất tiện này!
 		</div>
-		<video controls class="video-inpage">
-		  	<source src="public/assets/images/1.mp4" type="video/mp4">
-		</video>
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/PFBFn6aKjrc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 	</div>
 </body>
 <script type="text/javascript">
